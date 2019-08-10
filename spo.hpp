e8622a068dceaaf196c2f175d87030e44b6a57ec
Riccardo Massidda <contact@rmassidda.it>
2019-08-10 11:14:01 +0200

    Removed unused functions

--- spo.hpp
+++ spo.hpp
@@ -22,40 +22,10 @@ struct particle {
 };
 typedef struct particle particle_t;
 
-void update_vel ( float (&vel)[MAX_DIM], float loc_dist[MAX_DIM], float glb_dist[MAX_DIM], float a, float b, float c ){
-  // Random vals
-  std::pair<float,float> r;
-  for ( int i = 0; i < MAX_DIM; i ++ ) {
-    r.first = rnd();
-    r.second = rnd();
-  }
-  // Update vel
-  for ( int i = 0; i < MAX_DIM; i ++ ) {
-    vel[i] = a * vel[i];
-    vel[i] += r.first * b * loc_dist[i];
-    vel[i] += r.second * c * glb_dist[i];
-  }
-}
-
-void update_pos ( float (&pos)[MAX_DIM], const float vel[MAX_DIM] ) {
-  for ( int i = 0; i < MAX_DIM; i ++ ) {
-    pos[i] += vel[i];
-  }
-}
-
 void print_result ( FILE * file, result_t r ) {
   fprintf ( file, "( %.4f, %.4f ) -> %.4f\n",
       r.pos[0],
       r.pos[1],
       r.val );
-}
-
-// Random generates a position
-void init_particle ( particle_t& p ) {
-  for ( int i = 0; i < MAX_DIM; i++ ) {
-    p.vel[i] = rnd();
-    p.cur.pos[i] = rnd();
-    p.loc.pos[i] = p.cur.pos[i];
-  }
 }
 #endif
